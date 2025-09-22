d3c08b8edb7325c6d86d13dda9006764b730895d
Arjun Suwal <arjunsuwal900@gmail.com>
2025-09-22 17:08:56 -0400

    LoginSystem_Flask V 1.0.1 - Addde GET and POST Methods after submit route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,3 +13,11 @@ def about():
 @app.route("/contact")
 def contact():
     return "This is contact us page"
+
+@app.route("/submit",methods=["GET","POST"])
+def submit():
+    if request.method == "POST":
+        return "You have sent a data from the form."
+    
+    else:
+        return "You are just in the login page watching the form"
