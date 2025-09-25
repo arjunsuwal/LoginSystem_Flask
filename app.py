313859296fa5123d387f60837da2e8893a35fb91
Arjun Suwal <arjunsuwal900@gmail.com>
2025-09-24 20:28:57 -0400

    Mannual form handling with flask app

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request
 app= Flask(__name__)
 
 #creating the routes now
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/feedback", methods=["POST", "GET"])
+def feedback():
+    if request.method == "POST":
+        name = request.form.get("username")
+        message= request.form.get("message")
+        return render_template("thankyou.html", user=name, message=message)
+    return render_template("feedback.html")
 
 @app.route("/about")
 def about():
